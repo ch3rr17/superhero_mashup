2c38bba0d29772f804d5f243f2079c22c0b77bc7
ch3rr17 <cherr.batac@gmail.com>
2017-01-15 00:03:08 -0800

    added page content loader

--- src/js/movie.controller.js
+++ src/js/movie.controller.js
@@ -5,13 +5,14 @@
         .module('app')
         .controller('MovieController', MovieController);
 
-    MovieController.$inject = ['MovieFactory', '$log', '$stateParams'];
+    MovieController.$inject = ['MovieFactory', '$log', '$stateParams' ,'$timeout'];
 
     /* @ngInject */
-    function MovieController(MovieFactory, $log, $stateParams) {
+    function MovieController(MovieFactory, $log, $stateParams, $timeout) {
         var vm = this;
         vm.title = 'MovieController';
         vm.noMovies = false;
+        vm.loading = true;
 
         ////////////////
 
@@ -22,13 +23,25 @@
                 .then(
                     function(response) {
                         vm.movies = response.data.data.results.titles;
+                        $timeout(function(){
+                            console.log('here');
+                            vm.loading = false;
+                        }, 750);
+
+
+                        
                         if (vm.movies == null) {
                           vm.noMovies = true;
                         }
                     },
                     function(error) {
+                         $timeout(function(){
+                            vm.loading = false;
+                        }, 750);
+
                         $log.error(error);
                     });
+
         }
 
         vm.getMovies();
--- src/js/music.controller.js
+++ src/js/music.controller.js
@@ -5,10 +5,10 @@
         .module('app')
         .controller('MusicController', MusicController);
 
-    MusicController.$inject = ['MusicFactory', '$log', '$stateParams', '$sce'];
+    MusicController.$inject = ['MusicFactory', '$log', '$stateParams', '$sce', '$timeout'];
 
     /* @ngInject */
-    function MusicController(MusicFactory, $log, $stateParams, $sce) {
+    function MusicController(MusicFactory, $log, $stateParams, $sce, $timeout) {
         var vm = this;
         vm.title = 'MusicController';
 
@@ -16,7 +16,18 @@
         vm.noPhotos = false;
         vm.albumEmbedPlayer = '';
 
+        vm.musicLoaded = false;
+        vm.photosLoaded = false;
+
+        vm.loading = true;
+
         ////////////////
+
+        function hideLoader() {
+            $timeout(function() {
+                vm.loading = false;
+            }, 500);
+        }
 
         vm.movie = $stateParams.movie;
 
@@ -34,10 +45,16 @@
                         } else {
                           vm.albumEmbedPlayer = "https://embed.spotify.com/?uri=spotify%3Aalbum%3A" + vm.album.id;
                         }
+                        vm.musicLoaded = true;
+                        if(vm.photosLoaded){
+                            hideLoader();
+                        }
                     },
                     function(error) {
+                        hideLoader();
                         $log.error(error);
                     });
+
         };
 
         vm.getMusic();
@@ -46,12 +63,18 @@
         	MusicFactory.getLifeStyleForHero()
         		.then(
                     function(response) {
+                        // has music also loaded? then loading = false
                         vm.lifestyleImages = response.data.images;
                         if (vm.lifestyleImages.length === 0) {
                           vm.noPhotos = true;
                         }
+                        vm.photosLoaded = true;
+                        if(vm.musicLoaded){
+                            hideLoader();
+                        }
                     },
                     function(error) {
+                        hideLoader();
                         $log.error(error);
                     });
         };
